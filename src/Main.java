e317a55cccaca9b185a5b99f027bdd61689ee432
eltoonxz <eltnxz@gmail.com>
2026-03-14 11:54:01 -0300

    v.1.1

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         // LOGIN
-        String name = "admin";
-        String senha = "1234";
+        String name ;
+        String senha ;
 
         for (int tentativas = 0; tentativas <= 4; tentativas++) {
             System.out.println("USUARIO:");
@@ -51,6 +51,28 @@ public class Main {
                         System.out.println("VALOR DO DEPOSITADO R$" + valorDeposito);
                         break;
 
+                    case 3:
+                        for (int saque = 0; saque <= 3; saque++) {
+                            System.out.println(" SACAR ");
+                            System.out.println("DIGITE UM VALOR:");
+                            double valorSaque = sc.nextDouble();
+                            double saldoAtual = 2000;
+
+                            if (valorSaque <= 0 || valorSaque > saldoAtual) {
+                                System.out.println("IMPOSSIBILITADO DE EXECULTAR");
+
+                            } else {
+                                System.out.println("SAQUE");
+                                System.out.println("VALOR DO SAQUE R$" + valorSaque);
+                                break;
+                            }
+
+                        }
+                        break;
+
+                    case 4:
+                        System.out.println("OBRIGADO PELA CONFIANÇÃ");
+                        System.out.println("   SAINDO...");
 
 
                 }
